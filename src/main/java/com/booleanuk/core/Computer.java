f01e626d2182ff3de0ea95ec64b7c8540f26f964
Marit <marit.moe2@ntebb.no>
2024-01-19 09:21:34 +0100

    Exercise morning activity.

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,25 +3,33 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
-    public ArrayList<Game> installedGames = new ArrayList<>();
+    public ArrayList<Game> installedGames;
+    private PowerSupply powerSupply;
+
+    public Computer(PowerSupply ps) {
+        this.powerSupply = ps;
+        this.installedGames = new ArrayList<>();
+    }
+
+    public Computer(PowerSupply ps, ArrayList<Game> preInstalled) {
+        this.powerSupply = ps;
+        this.installedGames = preInstalled;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+        this.powerSupply.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String gameName) {
+        this.installedGames.add(new Game(gameName));
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
-
         return "Game not installed";
     }
 }
